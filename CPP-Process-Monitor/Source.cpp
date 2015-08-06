372353a13ad7c348e80871e1cc6644f61337c0b7
soachishti <soac@outlook.com>
2015-08-07 00:20:47 +0500

    Commented for while

--- CPP-Process-Monitor/Source.cpp
+++ CPP-Process-Monitor/Source.cpp
@@ -1,28 +1,30 @@
-#include <windows.h>
-#include <string>
-#include <map>
-#include <sstream>
-#include <vector>
-#include <iostream>
-#include <string>
-#include <iomanip>
-#include <algorithm>
-
-#include "CPdhQuery.h"
-#include "Func.hpp"
-
-#include "GetInfoPerfCount.hpp"
-
-using namespace std;
-
-void main()
-{
-	SetPriorityClass(GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS);
-	gotoxy(32, 10);
-	cout << "Loading...";
-
-	PerfCount::getInformation g; // Delay between query 1500 (1.5sec)
-	while (true) {
-		g.printData();
-	}
-}
+//#include <windows.h>
+//#include <string>
+//#include <map>
+//#include <sstream>
+//#include <vector>
+//#include <iostream>
+//#include <string>
+//#include <iomanip>
+//#include <algorithm>
+//
+//#include "CPdhQuery.h"
+//#include "Func.hpp"
+//
+//#include "GetInfoPerfCount.hpp"
+//
+//
+//
+//using namespace std;
+//
+//void main()
+//{
+//	SetPriorityClass(GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS);
+//	gotoxy(32, 10);
+//	cout << "Loading...";
+//
+//	PerfCount::getInformation g; // Delay between query 1500 (1.5sec)
+//	while (true) {
+//		g.printData();
+//	}
+//}
